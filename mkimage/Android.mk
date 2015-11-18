557f2973f4325f22754bc62075c3bbd8a9b40353
flintman <flintman@flintmancomputers.com>
2015-11-18 05:22:45 -0500

    Revert "mkimage for OMAP"
    Moved to system/core
    
    This reverts commit a20d9c58ba9edcb5e289f5e421d7b9e104b5838d.

--- mkimage/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := mkimage.c crc32.c
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE := mkimage
-
-include $(BUILD_HOST_EXECUTABLE)
